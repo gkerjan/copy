d24f6d5b77b54618389b41b019ca2c8ecdaabe42
gkerjan <gersende.kerjan@epfl.ch>
2025-10-07 00:17:51 +0200

    grjjr

--- GL3_env_cubemap/src/shaders/mirror.frag.glsl
+++ GL3_env_cubemap/src/shaders/mirror.frag.glsl
@@ -1,22 +1,4 @@
-precision mediump float;
-
-/* #TODO GL3.2.3
-	Setup the varying values needed to compue the Phong shader:
-	* surface normal
-	* view vector: direction to camera
-*/
-varying vec3 surface_normal;
-varying vec3 view_vector;
-
-uniform samplerCube cube_env_map;
-
-void main()
-{
-	/*
-	/* #TODO GL3.2.3: Mirror shader
-	Calculate the reflected ray direction R and use it to sample the environment map.
-	Pass the resulting color as output.
-	*/
+precision
 	vec3 r = reflect(view_vector, surface_normal);
 	vec4 result = textureCube(cube_env_map, r); 
 	vec3 color = result.xyz;
